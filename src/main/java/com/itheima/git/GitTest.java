dc944a672333a2f456da76110635cfa6f3008851
Upwind <3133145320@qq.com>
2022-04-27 23:20:45 +0800

    pull idea test

--- src/main/java/com/itheima/git/GitTest.java
+++ src/main/java/com/itheima/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git! master test");
         System.out.println("hello git! hot-fix test");
         System.out.println("push test");
+        System.out.println("pull to idea ago test");
     }
 }
